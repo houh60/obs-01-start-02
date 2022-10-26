955e2f262d2220b4283a42511d36003850c0dc16
William Hou <williamhou38@yahoo.com>
2022-10-26 01:12:56 -0500

    new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { UserService } from './user.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  userActivated = false;
-  constructor(
-    private userService: UserService
-  ) {}
+export class AppComponent implements OnInit, OnDestroy {
+    userActivated = false;
 
-  ngOnInit() {
-    this.userService.activated.subscribe(activated => this.userActivated = activated as boolean);
-  }
+    subscription = new Subscription;
+    constructor(
+        private userService: UserService
+    ) {}
+
+    ngOnInit() {
+        this.subscription = this.userService.activated
+            .subscribe(activated => this.userActivated = activated as boolean);
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
+    }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { filter, interval, map, Observable, Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-home',
@@ -12,8 +12,34 @@ export class HomeComponent implements OnInit, OnDestroy {
     constructor() {}
 
     ngOnInit() {
-        this.subscription = interval(1000).subscribe(count => {
-            console.log('count: ', count);
+        const customInterval = new Observable(observer => {
+            let count = 0;
+            setInterval(() => {
+                observer.next(count++);
+                if(count == 5) {
+                    observer.complete();
+                }
+                if(count > 3) {
+                    observer.error(new Error('Count is greater than 3!'));
+                }
+            }, 1000);
+        });
+
+        this.subscription = customInterval.pipe(filter(data => {
+            return data as number > 0
+        }), map(data => {
+            return 'Round: ' + (data as number + 1);
+        })).subscribe({
+            next: data => {
+                console.log('data: ', data);
+            },
+            error: error => {
+                console.log('error: ', error);
+                alert(error.message);
+            },
+            complete: () => {
+                console.log('Completed!');
+            }
         });
     }
 
